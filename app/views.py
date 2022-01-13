12e6755923270b5d5ea77492c24bfe1a7ffee9b2
yasmine1030 <yasminaasma1@gmail.com>
2022-01-13 22:31:06 +0000

    database

--- app/views.py
+++ app/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-
+from .models import *
 def store(request):
-	context= {}
+	products = Product.objects.all()
+	context = {'products': products}
 	return render(request, 'store/index.html', context)
 
 def cart(request):
