f8ac38fd2fdc779f3b0f127f508e4b5c2d0ea798
yasmine1030 <yasminaasma1@gmail.com>
2022-01-14 14:26:25 +0000

    add cart

--- app/models.py
+++ app/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
 class Customer(models.Model):
-    user = models.OneToOneField(User, null=True, blank=True, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
     name = models.CharField(max_length=200, null=True)
     email = models.CharField(max_length=200)
 
@@ -30,11 +30,26 @@ class Order(models.Model):
 	transaction_id = models.CharField(max_length=100, null=True)
 	def __str__(self):
 		return str(self.id)
+	@property
+	def get_cart_total(self):
+		orderitems = self.orderitem_set.all()
+		total = sum([item.get_total for item in orderitems])
+		return total
+	@property
+	def get_cart_items(self):
+		orderitems = self.orderitem_set.all()
+		total = sum([item.quantity for item in orderitems])
+		return total
 class OrderItem(models.Model):
 	product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
 	order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
 	quantity = models.IntegerField(default=0, null=True, blank=True)
 	data_added = models.DateTimeField(auto_now_add=True)
+	@property
+	def get_total(self):
+		total = self.product.price * self.quantity
+		return total
+
 
 class ShippingAdress(models.Model):
 	customer =models.ForeignKey(Customer, on_delete=models.SET_NULL, blank=True, null=True)
